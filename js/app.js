8db69c2319dad5f336d6257654f1ddb0f50bd5e4
KaryRQ <darkness.kary@hotmail.com>
2018-01-22 18:42:17 -0600

    vista splash y links

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/*vista splash */
+
+$().ready(function () {
+    $("#splash").fadeIn(0000).fadeOut(2000);
+});
+
+$(document).ready(function() {   
+    setTimeout(function() {
+        $("#container2").fadeIn(1500);
+    },2000);
+});

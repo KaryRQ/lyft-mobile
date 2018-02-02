862bb04a6158e5f852e1fe5ca11d30c694311f58
KaryRQ <darkness.kary@hotmail.com>
2018-02-02 17:53:21 -0600

    funcionalidad de register

--- js/register.js
+++ js/register.js
@@ -1,76 +1,36 @@
-$("#demo").intlTelInput();
+/*plugin de banderas al iniciar el documento */
+$(document).ready(function () {
+    $('#demo').intlTelInput();
+});
 
-// whether or not to allow the dropdown
-allowDropdown: true,
+/*se declara el valor del boton next */
+var next = $('.next');
+/*el botón estraa desactivado hasta que se introduzca un numero de 10 digitos */
+$("#demo").keypress(function () {
+    if ((this.value).length === 9) {
+        next.removeClass('disabled');
+    } else if ((this.value).length !== 10) {
+        next.addClass('disabled');
+    }
+});
 
-// if there is just a dial code in the input: remove it on blur, and re-add it on focus
-autoHideDialCode: true,
+/*función para crear código aleatorio */
+function makeid() {
+    var text = "";
+    var possible = "0123456789";
+    for (var i = 0; i < 5; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
 
-// add a placeholder in the input with an example number for the selected country
-autoPlaceholder: "polite",
+    return text;
+}
+console.log('makeid');
 
-// modify the auto placeholder
-customPlaceholder: null,
+/*alert que desplegará el código aleatorio generado y nos redirecciona a la siguiente pantalla */
+next.on("click", function (event) {
+    event.preventDefault();
+    makeCode();
+    alert('Tu código es: ' + 'LAB-' + code);
+    window.location.href = 'index.html';
+});
 
-// append menu to a specific element
-dropdownContainer: "",
 
-// don't display these countries
-excludeCountries: [],
-
-// format the input value during initialisation
-formatOnDisplay: true,
-
-// geoIp lookup function
-geoIpLookup: null,
-
-// initial country
-initialCountry: "",
-
-// don't insert international dial codes
-nationalMode: true,
-
-// number type to use for placeholders
-placeholderNumberType: "MOBILE",
-
-// display only these countries
-onlyCountries: [],
-
-// the countries at the top of the list. defaults to united states and united kingdom
-preferredCountries: [ "us", "gb" ],
-
-// display the country dial code next to the selected flag so it's not part of the typed number
-separateDialCode: false,
-
-// specify the path to the libphonenumber script to enable validation/formatting
-utilsScript: ""
-
-// destroy
-$("#demo").intlTelInput("destroy");
-
-// Get the extension part of the current number
-var extension = $("#demo").intlTelInput("getExtension");
-
-// Get the current number in the given format
-var intlNumber = $("#demo").intlTelInput("getNumber");
-
-// Get the type (fixed-line/mobile/toll-free etc) of the current number. 
-var numberType = $("#demo").intlTelInput("getNumberType");
-
-// Get the country data for the currently selected flag.
-var countryData = $("#demo").intlTelInput("getSelectedCountryData");
-
-// Get more information about a validation error. 
-var error = $("#demo").intlTelInput("get<a href="https://www.jqueryscript.net/tags.php?/Validation/">Validation</a>Error");
-
-// Vali<a href="https://www.jqueryscript.net/time-clock/">date</a> the current number
-var isValid = $("#demo").intlTelInput("isValidNumber");
-
-// Load the utils.js script (included in the lib directory) to enable formatting/validation etc.
-$("#demo").intlTelInput("loadUtils", "lib/libphonenumber/build/utils.js");
-
-// Change the country selection
-$("#demo").intlTelInput("selectCountry", "gb");
-
-// Insert a number, and update the selected flag accordingly.
-$("#demo").intlTelInput("setNumber", "+44 7733 123 456");
